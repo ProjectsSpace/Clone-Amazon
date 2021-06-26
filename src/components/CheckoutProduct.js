c77977471942a41619428c15794c9741850098b4
Mocarram Hosan <mocarramhosan@gmail.com>
2021-06-27 00:36:23 +0600

    cart done

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useStateValue } from "../StateProvider";
+
+function CheckoutProduct({ id, image, title, rating, price }) {
+  const [{ cart }, dispatch] = useStateValue();
+  const removeFromCart = () => {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: id,
+    });
+  };
+
+  return (
+    <div className="checkout__item-row">
+      <figure className="item__image">
+        <img src={image} alt={title} />
+      </figure>
+      <div className="item__title">
+        <p>
+          <strong>{title}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill(0)
+            .map(() => (
+              <span key={uuidv4()}>⭐</span>
+            ))}{" "}
+          <strong>{rating}</strong>
+        </div>
+        <div className="remove__item-button">
+          <button onClick={removeFromCart}>Remove item</button>
+        </div>
+      </div>
+      <div className="item__price">
+        <strong>{price}</strong>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
