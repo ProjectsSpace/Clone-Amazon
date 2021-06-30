532af7782fdc22be6385ee45493afe947f22a2db
Mocarram Hosan <mocarramhosan@gmail.com>
2021-06-30 18:54:53 +0600

    express backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // The API (cloud functions) url
+  baseURL: "https://amazon-clone-be.herokuapp.com/api", // The API (cloud functions) url
 });
 
 export default instance;
