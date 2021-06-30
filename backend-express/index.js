5fe5dcc148d7d7c1dbd9b94ceacd288c4056f690
Mocarram Hosan <mocarramhosan@gmail.com>
2021-06-30 22:12:32 +0600

    express backend

---
+++ backend-express/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const stripe = require("stripe")(
+  "sk_test_51J7MgySG1N17uyNow49HA6xwiwuT7hSUnyBj3J6ZkBqjvbHAfUVAyr64JArPz05ifF2QbRO6RhpAr6tspUrI0kwX00PrTDgjhD"
+);
+
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Mocarram");
+});
+
+app.post("/api/payments/create", async (req, res) => {
+  const total = req.query.total;
+
+  console.log("payment request received, yeeeee!!!", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "INR",
+    description: "Amazon Clone Payments",
+  });
+
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Listen command
+const port = process.env.PORT || "3000";
+app.listen(port, () => {
+  console.log(`server started on port:- ${port}`);
+});
